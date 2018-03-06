aa2b25728daba398a10585127a5fdbf875d9a64a
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-06 23:06:01 +0700

    kurang koma

--- tugas1/urls.py
+++ tugas1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'v1/login/$',views.login, name="login")
-    re_path(r'v1/users/$',views.users, name="get-list-of-users")
+    re_path(r'v1/login/$',views.login, name="login"),
+    re_path(r'v1/users/$',views.users, name="users"),
     #re_path(r'v1/comments/(?:(?P<id>\d+))?$',views.comments, name="comments")
 ]

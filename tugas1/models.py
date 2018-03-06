1c704cce3d27c6b4ac398822beba06f40b49b1e8
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-06 23:44:29 +0700

    model user dan ubah setting

--- tugas1/models.py
+++ tugas1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=40)
+    displayName = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.username

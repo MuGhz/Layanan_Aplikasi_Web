44db9163dbfa447905e62ada9aac2c8281341052
root <Muhammad Ghozi>
2018-03-07 05:01:11 +0000

    merge

--- tugas1/models.py
+++ tugas1/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=40)
     displayName = models.CharField(max_length=40)
-
     def __str__(self):
         return self.username

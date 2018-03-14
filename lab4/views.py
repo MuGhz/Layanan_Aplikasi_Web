2ccdeac95918754c9ac2688f7831de11ca2b4cce
root <Muhammad Ghozi>
2018-03-14 02:56:51 +0000

    image done, thumbnail not yet

--- lab4/views.py
+++ lab4/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Lab4 - Muhammad Ghozi")
+    image_data = open('lab4/charizad.jpg','rb').read()
+    return HttpResponse(image_data, content_type='image/jpeg')
 def thumbnail(request):
-    return HttpResponse("this is thumbnail")
+    return HttpResponse()

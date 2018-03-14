edee2744399ac8739874d94f43eadd608f18bb61
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-14 12:49:32 +0700

    MK 999999

--- lab4/views.py
+++ lab4/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from PIL import Image
-
+import os
 # Create your views here.
 def index(request):
     image_data = open('lab4/image/charizad.jpg','rb').read()
     return HttpResponse(image_data, content_type='image/jpeg')
 def thumbnail(request):
-    response = Image.open('lab4/image/charizad.jpg')
-    response = response.thumbnail((300,300))
-    response = response.save("thumbnail_%s_%s" % ('lab4/image/charizad.jpg', "_".join(size)))
+    filename = os.path.dirname(__file__)+'/image/charizad'
+    thumbnail_file = filename+'-thumbnail.jpg'
+    size = 300,300
+    response = Image.open(filename+'.jpg')
+    response = response.thumbnail(size)
+    response = response.save(filename+'-thumbnail.jpg',"JPEG")
     return HttpResponse(response)

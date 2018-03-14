66dcd988a7c902a3467105a0824fd97bc17c24bf
root <Muhammad Ghozi>
2018-03-14 04:54:39 +0000

    imageee thumbnail

--- lab4/views.py
+++ lab4/views.py
@@ -8,4 +8,4 @@ def index(request):
 def thumbnail(request):
     image_data = open('lab4/charizad.jpg','rb').read()
     im = get_thumbnail(image_data,'300x300',crop='center',quality=99)
-    return HttpResponse(im, content_type='image/jpeg')
+    return HttpResponse(im)

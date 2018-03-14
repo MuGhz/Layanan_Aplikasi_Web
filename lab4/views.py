f0c40e0bc769f3f52b0c56e769d9f3777724f05e
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-14 12:09:22 +0700

    huehuehuehue

--- lab4/views.py
+++ lab4/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from sorl.thumbnail import get_thumbnail
+from django.template import loader
+
 # Create your views here.
 def index(request):
     image_data = open('lab4/charizad.jpg','rb').read()
     return HttpResponse(image_data, content_type='image/jpeg')
 def thumbnail(request):
+    template = loader.get_template('lab4/index.html')
     image_data = open('lab4/charizad.jpg','rb').read()
     im = get_thumbnail(image_data,'300x300',crop='center',quality=99)
-    return HttpResponse(im)
+    context={
+        'item':im,
+    }
+    return render(template.render(context,request))

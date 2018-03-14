4ca76f09f68b367015fe1745f632558ea3aae4b5
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-14 10:30:16 +0700

    thumbnails

--- lab4/views.py
+++ lab4/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from thumbnails import get_thumbnail
 # Create your views here.
 def index(request):
     image_data = open('lab4/charizad.jpg','rb').read()
     return HttpResponse(image_data, content_type='image/jpeg')
 def thumbnail(request):
-    return HttpResponse()
+    return HttpResponse(get_thumbnail('lab4/charizad.jpg', '300x300', crop='center'),content_type='image/jpeg')

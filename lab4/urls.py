0da0443826a233f2f9c1494df1b86cf6e92e8a90
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-03-23 22:22:07 +0700

    add function mk1

--- lab4/urls.py
+++ lab4/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('thumbnail/', views.thumbnail, name='thumbnail'),
+    path('add/', views.add, name='add'),
     #re_path(r'v1/comments/(?:(?P<id>\d+))?$',views.comments, name="comments")
 ]
--- lab4/views.py
+++ lab4/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from PIL import Image
+from datetime import datetime
 import os
 # Create your views here.
 def index(request):
@@ -16,3 +17,35 @@ def thumbnail(request):
     return HttpResponse(open(thumbnail_file,'rb'), content_type='image/jpeg')
 def add(request):
     if request.method == 'GET' :
+        a = request.GET.get('a')
+        b = request.GET.get('b')
+        data = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " :: A = " + a + " :: B = " + b
+        logfile = os.path.dirname(__file__)+'/log/logfile.txt'
+        log = open(logfile, 'a')
+        log.write(data)
+        log.close()
+        if isinstance(a,int) and isinstance(b,int) :
+            sum = a+b
+            response = {
+                'status': '200',
+                'hasil': sum
+            }
+            return JsonResponse(response,status=200)
+        elif not isinstance(a,int) :
+            response = {
+                'status': '400',
+                'description': 'Bad Request. a is not integer'
+            }
+            return JsonResponse(response,status=400)
+        elif not isinstance(b,int) :
+            response = {
+                'status': '400',
+                'description': 'Bad Request. b is not integer'
+            }
+            return JsonResponse(response,status=400)
+    else :
+        response = {
+            'status': '400',
+            'description': 'Bad Request'
+        }
+        return JsonResponse(response,status=400)

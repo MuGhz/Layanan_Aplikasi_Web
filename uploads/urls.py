aa088fde54b5a8b371a9865bd133cba2c9f9e270
root <Muhammad Ghozi>
2018-04-07 13:43:11 +0000

    refactor js

--- uploads/urls.py
+++ uploads/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test', views.test, name='test'),
+    path('upload-progress.js', views.javascript, name='javascript'),
     #re_path(r'v1/comments/(?:(?P<id>\d+))?$',views.comments, name="comments")
 ]

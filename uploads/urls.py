48574fa70aeddebfb3b539d92e574b707236be5a
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-04-07 19:06:31 +0700

    coba contoh scele upload

--- uploads/urls.py
+++ uploads/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test', views.test, name='test'),
-    path('upload.php', views.upload, name='upload'),
+    path('upload', views.upload, name='upload'),
     #re_path(r'v1/comments/(?:(?P<id>\d+))?$',views.comments, name="comments")
 ]
--- uploads/views.py
+++ uploads/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import os
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. Welcome to Muhammad Ghozi's webservices.")
@@ -7,4 +11,9 @@ def index(request):
 def test(request):
     return render(request,'uploads/uploads.html')
 def upload(request):
-    return render(request, 'uploads/upload.php')
+    folder = filename = os.path.dirname(__file__)+'/templates/uploads/cache'
+    myfile = request.FILES['file']
+    fs = FileSystemStorage(location=folder)
+    filename = fs.save(myfile.name, myfile)
+    uploaded_file_url = fs.url(filename)
+    return render(request, 'uploads/uploads.html')

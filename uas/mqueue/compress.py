1c443d75515a1f497688eef007fb12af5bed2acd
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2018-06-02 13:06:17 +0700

    bismillahhirahmannirahim

--- uas/mqueue/compress.py
+++ uas/mqueue/compress.py
@@ -1,4 +1,6 @@
-import pika, json, zipstream, time
+import pika, json, zipstream, time, os
+
+RESOURCE_URL = 'http://172.22.0.2/oauth/resource'
 credentials = pika.PlainCredentials('1406559055', '1406559055')
 params= pika.ConnectionParameters('152.118.148.103',5672,'/1406559055',credentials)
 connection = pika.BlockingConnection(params)
@@ -8,13 +10,26 @@ result = channel.queue_declare()
 queue_name = result.method.queue
 channel.queue_bind(exchange='ZIP_QUEUE',queue=queue_name,routing_key='')
 print ('[X] Waiting for logs')
+
+def authorize(bearer):
+    print("BEARER = ",bearer)
+    headers = {}
+    headers['Authorization'] = bearer
+    response = requests.get(RESOURCE_URL,headers=headers)
+    return response
+
 def zip_file(ch, method, properties, body):
     time.sleep(5)
     try :
         msg = json.loads(body.decode("utf-8"))
-        location = msg['file']
-        size = msg['size']
         fname = msg['fname']
+        location = os.path.dirname(__file__)+'/templates/uas/cache/'+fname
+        size = msg['size']
+        token = msg['token']
+    except Exception as e:
+        print ("[E] Error :",e)
+    try :
+        response = authorize(token)
         sum = 0
         z = zipstream.ZipFile(mode='w', compression=zipstream.ZIP_DEFLATED)
         z.write(msg["file"])
@@ -27,7 +42,7 @@ def zip_file(ch, method, properties, body):
                 channel.basic_publish(exchange='ZIP_QUEUE',routing_key=fname,body=str(sum))
             print("[X] compress done")
     except Exception as e:
-        print ("[E] Error :",e)
+        print("Unauthorized")
 
 channel.basic_consume(zip_file, queue=queue_name, no_ack=True)
 channel.start_consuming()
--- uas/mqueue/orchestrator.py
+++ uas/mqueue/orchestrator.py
@@ -1,9 +1,25 @@
 import pika,json
+
+import requests
+
+CLIENT_ID = '2Byfk7psd7nEc836XHGZ3ctZLsehIHPQ'
+CLIENT_SECRET = 'nM33J6jYIWrJw8W8qM9RVEyBxWqcDdbD'
+GRANT_TYPE = 'password'
+OAUTH_URL = 'http://172.22.0.2/oauth/token'
+RESOURCE_URL = 'http://172.22.0.2/oauth/resource'
+
+def get_token(username,password):
+    payloads = {'username': username, 'password': password, 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET, 'grant_type': GRANT_TYPE}
+    response = requests.post(OAUTH_URL, data=payloads)
+    return response
+
 credentials = pika.PlainCredentials('1406559055', '1406559055')
 params= pika.ConnectionParameters('152.118.148.103',5672,'/1406559055',credentials)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.exchange_declare(exchange='ORC_CREDENTIALS', exchange_type='fanout')
+channel_compress = connection.channel()
+channel_compress.exchange_declare(exchange="ZIP_QUEUE", exchange_type='direct', durable=True)
 result = channel.queue_declare()
 queue_name = result.method.queue
 channel.queue_bind(exchange='ORC_CREDENTIALS',queue=queue_name,routing_key='')
@@ -16,6 +32,8 @@ def orches(ch, method, properties, body):
         password = msg['password']
         fname = msg['filename']
         size = msg['size']
+        token = get_token(username,password)
+        channel_compress.basic_publish(exchange='ZIP_QUEUE',routing_key='',body={'filename':fname,'token':token,'size':size})
     except Exception as e:
         print ("[E] Error :",e)
 
